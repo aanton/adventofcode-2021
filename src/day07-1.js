846cc97dab250ac0e97c404f8bd5fb93a85974fa
aanton <aanton@gmail.com>
2021-12-07 15:39:33 +0100

    Day 7 challenges

---
+++ src/day07-1.js
@@ -0,0 +1,26 @@
+import {readFileSync} from 'fs'
+
+const {pathname} = new URL('../inputs/day07.txt', import.meta.url)
+const array = readFileSync(pathname).toString().split('\n')
+console.log(`Number of lines: ${array.length}`)
+
+const positions = array[0].split(',').map(position => parseInt(position))
+console.log(`Number of positions: ${positions.length}`)
+
+const countByPosition = positions.reduce((acc, position) => {
+  acc[position] = acc[position] ? acc[position] + 1 : 1
+
+  return acc
+}, [])
+
+const distanceByPosition = new Array(countByPosition.length)
+  .fill(0)
+  .map((_, index) => {
+    return countByPosition.reduce((acc, count, position) => {
+      const distance = Math.abs(position - index)
+      acc = acc + distance * count
+      return acc
+    }, 0)
+  })
+
+console.log(Math.min(...distanceByPosition))
