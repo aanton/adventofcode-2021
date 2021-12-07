3e7fca07e1f522d8b506f14a51b38b1c52834d4f
aanton <aanton@gmail.com>
2021-12-07 11:26:24 +0100

    Day 6 challenges

---
+++ src/day06.js
@@ -0,0 +1,24 @@
+import {readFileSync} from 'fs'
+
+const {pathname} = new URL('../inputs/day06.txt', import.meta.url)
+const array = readFileSync(pathname).toString().split('\n')
+console.log(`Number of lines: ${array.length}`)
+
+const fishes = array[0].split(',').map(fish => parseInt(fish))
+console.log(`Number of fishes: ${fishes.length}`)
+
+const countFishes = function (fishes, days) {
+  let fishesByAge = new Array(9).fill(0)
+
+  fishes.forEach(age => (fishesByAge[age] += 1))
+
+  for (let i = 0; i < days; i++) {
+    const [age0, age1, age2, ag3, age4, age5, age6, age7, age8] = fishesByAge
+    fishesByAge = [age1, age2, ag3, age4, age5, age6, age7 + age0, age8, age0]
+  }
+
+  return fishesByAge.reduce((acc, value) => acc + value, 0)
+}
+
+console.log('Fishes after 80 days', countFishes(fishes, 80))
+console.log('Fishes after 80 days', countFishes(fishes, 256))
