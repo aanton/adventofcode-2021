322ee3b74be82df81cd064cc53bb81ed7796ef30
aanton <aanton@gmail.com>
2021-12-15 20:41:45 +0100

    Day 15 challenges (part 1)

---
+++ src/utils/dijkstra-graph.js
@@ -0,0 +1,94 @@
+// @see https://medium.com/@adriennetjohnson/a-walkthrough-of-dijkstras-algorithm-in-javascript-e94b74192026
+
+export default class Graph {
+  constructor() {
+    this.nodes = []
+    this.adjacencyList = {}
+  }
+
+  addNode(node) {
+    this.nodes.push(node)
+    this.adjacencyList[node] = []
+  }
+
+  addEdge(node1, node2, weight) {
+    this.adjacencyList[node1].push({node: node2, weight: weight})
+    // Weights are different in each direction so edges are inserted in both directions
+    // this.adjacencyList[node2].push({node:node1, weight: weight});
+  }
+
+  findPathWithDijkstra(startNode, endNode) {
+    let times = {}
+    let backtrace = {}
+    let pq = new PriorityQueue()
+
+    times[startNode] = 0
+
+    this.nodes.forEach(node => {
+      if (node !== startNode) {
+        times[node] = Infinity
+      }
+    })
+
+    pq.enqueue([startNode, 0])
+
+    while (!pq.isEmpty()) {
+      let shortestStep = pq.dequeue()
+      let currentNode = shortestStep[0]
+      this.adjacencyList[currentNode].forEach(neighbor => {
+        let time = times[currentNode] + neighbor.weight
+
+        if (time < times[neighbor.node]) {
+          times[neighbor.node] = time
+          backtrace[neighbor.node] = currentNode
+          pq.enqueue([neighbor.node, time])
+        }
+      })
+    }
+
+    let path = [endNode]
+    let lastStep = endNode
+    while (lastStep !== startNode) {
+      path.unshift(backtrace[lastStep])
+      lastStep = backtrace[lastStep]
+    }
+
+    return {
+      path,
+      time: times[endNode],
+    }
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.collection = []
+  }
+
+  enqueue(element) {
+    if (this.isEmpty()) {
+      this.collection.push(element)
+    } else {
+      let added = false
+      for (let i = 1; i <= this.collection.length; i++) {
+        if (element[1] < this.collection[i - 1][1]) {
+          this.collection.splice(i - 1, 0, element)
+          added = true
+          break
+        }
+      }
+      if (!added) {
+        this.collection.push(element)
+      }
+    }
+  }
+
+  dequeue() {
+    let value = this.collection.shift()
+    return value
+  }
+
+  isEmpty() {
+    return this.collection.length === 0
+  }
+}
